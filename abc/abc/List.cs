c8ac7683e7b1a51055c81079f23b2b97582566a9
vohoangchinh <vohoangchinh1996@gmail.com>
2016-09-26 09:14:08 +0700

    abc

---
+++ abc/abc/List.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abc
+{
+    class List<T>
+    {
+        element<T> head;
+
+        internal element<T> Head
+        {
+            get { return head; }
+            set { head = value; }
+        }
+        public List()
+        {
+        }
+        public void addfirst(element<T> e)
+        {
+            e.Next = head;
+            this.head = e;
+        }
+    }
+}
