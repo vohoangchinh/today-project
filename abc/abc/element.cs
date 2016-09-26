3806e94a84aa8806308f80b62c5dfa15da8efeca
vohoangchinh <vohoangchinh1996@gmail.com>
2016-09-26 09:03:04 +0700

    df

---
+++ abc/abc/element.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abc
+{
+    class element<T>
+    {
+        private T data;
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        element<T> next;
+        internal element<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+    }
+}
